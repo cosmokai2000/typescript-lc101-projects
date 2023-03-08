d58e5c4c9ca7acc9bc737a410aaa81f11167ef48
Stephen Cleveland <cosmokai2000@gmail.com>
2023-03-08 10:25:21 -0600

    finished exercises late

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -1,15 +1,19 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
-
+import { SpaceLocation } from './SpaceLocation';
 // Part 1: Declare (5) Variables With Type
-
+let spacecraftName: string = 'Determination';
+let speedMph: number = 17500;
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number = 0.621;
 
 
 // Part 2: Print Days to Mars
-
-
-
+let milesToMars: number = kilometersToMars*milesPerKilometer
+let hoursToMars: number = milesToMars/speedMph
+let daysToMars: number = hoursToMars/24
 // Code an output statement here (use a template literal):
 
 
@@ -23,19 +27,38 @@
 
 
 
-
 // Part 4: Create a Spacecraft Class
 
-
+class Spacecraft {
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
+ 
+    constructor(name: string, speedMph: number) {
+       this.name = name;
+       this.speedMph = speedMph;
+    }
+ 
+    getDaysToLocation(kilometersAway: number): number {
+       let milesAway: number = kilometersAway * this.milesPerKilometer;
+       let hoursToMars: number = milesAway / this.speedMph;
+       return hoursToMars / 24;
+    }
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+     }
+ }
+ 
 
 
 // Create an instance of the class here:
 
-
+let spaceShuttle = new Spacecraft('Determination', 17500);
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-
+spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
 
 
 // Part 5: Export and Import the SpaceLocation Class
@@ -45,3 +68,5 @@
 // Add the printDaysToLocation function to the Spacecraft class.
 
 // Paste in the code from step 6 here:
+Determination would take 332.67857142857144 days to get to Mars.
+Determination would take 0.5683628571428571 days to get to the Moon.
